e29b249c7b5c640b6e90ce3c2fd47355ddef7bf8
DadeCoder <dade_zhang@163.com>
2016-11-10 17:33:22 +0800

    dubbo consumer init

--- Consume/src/main/java/com/dade/consume/ConsumeApplication.java
+++ Consume/src/main/java/com/dade/consume/ConsumeApplication.java
@@ -1,8 +1,16 @@
 package com.dade.consume;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
+@ComponentScan                                                //组件扫描
+@Configuration                                                //配置控制
+@EnableAutoConfiguration                                    //启用自动配置
+@ImportResource({"classpath:dubbo-consumer.xml"}) 			//加入spring的bean的xml文件
 @SpringBootApplication
 public class ConsumeApplication {
 
--- Consume/src/main/java/com/dade/consume/controller/UserController.java
+++ Consume/src/main/java/com/dade/consume/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.dade.consume.controller;
 
+import com.dade.providerApi.UserRpc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Dade on 2016/11/10.
@@ -10,11 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v2/consumer")
 public class UserController {
 
+    @Resource
+    UserRpc userRpc;
+
     @RequestMapping("/test")
     String test(){
         String res = "I'm Consumer!";
         return res;
     }
+
+    @RequestMapping("/bubbo")
+    String bizDubbo(){
+        String res = userRpc.getUserName();
+        return res;
+    }
+
 
 
 }
